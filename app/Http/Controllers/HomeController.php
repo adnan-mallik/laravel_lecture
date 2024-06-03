e8ca05ddbaf72eebd42fe33d764ae47f0ca70b78
casa-apps <adnan@casaapps.com>
2024-06-03 16:21:07 +0500

    XML HTTP REQUEST

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Student;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;;
 
 class HomeController extends Controller
 {
@@ -24,17 +25,32 @@ class HomeController extends Controller
     }
 
     public function userRegister() {
-        $request = request();
+        // dd(request()->all());
+        $request = request()->all();
         $rules = [
             'first_name' => 'required|min:3|max:30',
             'last_name'  => 'required|min:3|max:30',
-            'email'      => 'required|email',
+            'email'      => 'required|email|unique:students,email',
             'password'   => 'required|min:8',
         ];
         $custom_messages = [
             "first_name.required" => "First name can not be null"
         ];
-        $validated = $request->validate($rules, $custom_messages);
+        // $validated = $request->validate($rules, $custom_messages);
+
+        $validator = Validator::make($request, $rules, $custom_messages); // scope resulution operator
+        // dd($validator->fails());
+
+        if( $validator->fails() ){
+            return response()->json([
+                "status"    => "errors",
+                "messages"  => $validator->errors()
+            ]);
+        }
+
+        // Retrieve the validated input...
+        $validated = $validator->validated();
+
         // dd($validated);
         // request();
         // $validated = $request->validate([
@@ -54,15 +70,21 @@ class HomeController extends Controller
         // ]
         // );
 
-        $student = new Student;
+        // $student = new Student;
+        $student = Student::firstOrNew(['email' => $validated["email"]]);
         
-        $student->first_name    = $request->first_name;  
-        $student->last_name     = $request->last_name;  
-        $student->email         = $request->email;  
-        $student->password      = $request->password;
+        // $student->first_name    = $request->first_name;  
+        $student->last_name     = $validated["last_name"];
+        $student->email         = $validated["email"];  
+        $student->password      = $validated["password"];
 
         if($student->save()):
-            return back()->withSuccess("Student Record is created");
+            // return back()->withSuccess("Student Record is created");
+
+            return response()->JSON([
+                "status"  => "success",
+                "message" => "student created"
+            ]);
         endif;
         // dd( $student->user );
     }
--- resources/views/Auth/register.blade.php
+++ resources/views/Auth/register.blade.php
@@ -32,10 +32,11 @@
                 @endif --}}
                 @if(session('success'))
                     <div class="alert alert-success">
-                        {{session('success')}}
+                        {{ session('success') }}
                     </div>
                 @endif
-                <form action="{{ route('user.register') }}" method="post">
+                {{-- <form action="{{ route('user.register') }}" method="post"> --}}
+                <form id="student_data">
                     @csrf
                     <div class="mb-3">
                         <label for="first_name" class="form-label">First Name</label>
@@ -43,25 +44,59 @@
                     </div>
                     <div class="mb-3">
                         <label for="last_name" class="form-label">Last Name</label>
-                        <input type="text" name="last_name" class="form-control" id="last_name" />
+                        <input type="text" name="last_name" class="form-control" id="last_name" value="{{ old('last_name') }}" />
                     </div>
                     <div class="mb-3">
                         <label for="email" class="form-label">Email address</label>
-                        <input type="email" name="email" class="form-control" id="email">
+                        <input type="email" name="email" class="form-control" id="email" value="{{ old('email') }}">
                         <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
                     </div>
                     <div class="mb-3">
-                        <label for="exampleInputPassword1" class="form-label">Password</label>
-                        <input type="password" name="password" class="form-control" id="exampleInputPassword1">
+                        <label for="password" class="form-label">Password</label>
+                        <input type="password" name="password" class="form-control" id="password" value="{{ old('email') }}">
                     </div>
                     <div class="mb-3 form-check">
                         <input type="checkbox" name="check_out" class="form-check-input" id="exampleCheck1">
                         <label class="form-check-label" for="exampleCheck1">Check me out</label>
                     </div>
-                    <button type="submit" class="btn btn-primary">Register</button>
+                    {{-- <button type="submit" class="btn btn-primary">Register</button> --}}
+                    <button type="button" class="btn btn-primary" id="student_register_btn">Register</button>
                 </form>
             </div>
         </div>
     </div>
+
+
+    <SCript>
+
+        let student_register_btn = document.getElementById('student_register_btn');
+
+        student_register_btn.addEventListener('click', function(){
+            
+            let csrfToken = "{{ csrf_token() }}";
+
+
+            var data = new FormData();
+            data.append('first_name', document.getElementById('first_name').value);
+            data.append('last_name', document.getElementById('last_name').value);
+            data.append('email', document.getElementById('email').value);
+            data.append('password', document.getElementById('password').value);
+
+            let xhttp = new XMLHttpRequest();
+
+            xhttp.onload = function() {
+                // logic after the response is ready
+            }
+
+            xhttp.open("post", "{{ route('user.register') }}")
+            xhttp.setRequestHeader('X-CSRF-TOKEN', csrfToken);
+            xhttp.send(data);
+
+        });
+
+
+
+
+    </SCript>
 </body>
 </html>
